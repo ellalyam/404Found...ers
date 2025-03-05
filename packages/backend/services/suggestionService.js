7f1989d4762f2ee896ca2eea779d37ed23b8b125
Gwendolyn Wren <137141797+0x4777656e@users.noreply.github.com>
2025-03-05 08:14:48 -0800

    reformat recommendation JSON

--- packages/backend/services/suggestionService.js
+++ packages/backend/services/suggestionService.js
@@ -1,31 +1,54 @@
 import spotifyServices from "./spotifyServices.js";
 
 /**
+ * Gets emotion with the highest value from emotion JSON (ie most present in facial detection)
+ * @param {JSON} emotion - From Hume (or user form?)
+ */
+function getMainEmotion(emotion) {
+    return emotion
+      .reduce((max, em) => em.score > max.score ? em : max, emotion[0])
+      .name;
+}
+
+/**
   * Gets recommended songs with the given seed and parameters
-  * @param  {JSON}    seed        Artist IDs and song parameters
-  * @return {Array<String>}       Array of Spotify track IDs
+  * @param  {JSON}  seed  Artist IDs and song parameters
+  * @return {JSON}        Suggestion
   */
 async function getRecommendedTracks(seed) {
   const url = "https://api.reccobeats.com/v1/track/recommendation?";
   const response = await fetch(url + new URLSearchParams({
     size: 5,
     ...seed,
-  }), {
+  }),
+  {
     headers: {
       "Accept": "application/json",
     },
   });
 
   const responseData = await response.json();
-  return responseData.content.map((track) => track.href.substring(31));
+  const outputData = {
+    mood: getMainEmotion(seed),
+    dateSuggested: new Date().toLocaleDateString(),
+    tracks: responseData.content.map((track) => {
+      return {
+        title: track.trackTitle,
+        id: track.href.substring(31),
+        artist: track.artists[0],
+      }
+    }),
+  }
+  return outputData
+  //return responseData.content.map((track) => track.href.substring(31));
 }
 
 /**
   * Gets track suggestions from ReccoBeats API
-  * @param   {String}  accessToken Spotify user's API access token
-  * @param   {JSON}    songParams  Target values for danceability, energy,
-  *                                speechiness, and valence
-  * @return  {Array<String>}       Array of Spotify track IDs
+  * @param   {String}  accessToken  Spotify user's API access token
+  * @param   {JSON}    songParams   Target values for danceability, energy,
+  *                                 speechiness, and valence
+  * @return  {JSON}                 Suggestion
   */
 async function getSuggestions(accessToken, songParams) {
   // Get user's top tracks and add to seed
